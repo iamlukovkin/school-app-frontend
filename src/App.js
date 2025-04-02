1a0262e0847047005d255097dffa144f6b321410
Ivan Lukovkin <iamlukovkin@Ivans-MacBook-Pro.local>
2025-04-02 17:26:13 +0300

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from "./components/Header";
 import Main from "./pages/Main";
-import {Route, Router, Routes} from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import RoadRules from "./pages/RoadRules";
 import HelpPhones from "./pages/HelpPhones";
 import Scammers from "./pages/Scammers";
@@ -10,26 +10,30 @@ import Extremism from "./pages/Extremism";
 import About from "./pages/About";
 import Drones from "./pages/Drones";
 import SafetyNetwork from "./pages/SafetyNetwork";
+import PinnedElement from './components/PinnedElement';
 
 function App() {
-    return (<>
-        <Header/>
-        <div className={"app"}>
-            <Router location={window.location} navigator={window.history}>
-                <Routes>
-                    <Route exact index={true} path={Main.link} element={<Main/>}/>
-                    <Route path={SafetyNetwork.link} element={<SafetyNetwork/>}/>
-                    <Route path={RoadRules.link} element={<RoadRules/>}/>
-                    <Route path={HelpPhones.link} element={<HelpPhones/>}/>
-                    <Route path={Scammers.link} element={<Scammers/>}/>
-                    <Route path={Corruption.link} element={<Corruption/>}/>
-                    <Route path={Extremism.link} element={<Extremism/>}/>
-                    <Route path={About.link} element={<About/>}/>
-                    <Route path={Drones.link} element={<Drones/>}/>
-                </Routes>
-            </Router>
-        </div>
-    </>);
+  return (<>
+    <Header />
+    <main>
+      <PinnedElement />
+      <Router location={window.location} navigator={window.history}>
+        <Routes>
+          <Route exact index={true} path={Main.link} element={<Main />} />
+          <Route path={SafetyNetwork.link} element={<SafetyNetwork />} />
+          <Route path={RoadRules.link} element={<RoadRules />} />
+          <Route path={HelpPhones.link} element={<HelpPhones />} />
+          <Route path={Scammers.link} element={<Scammers />} />
+          <Route path={Corruption.link} element={<Corruption />} />
+          <Route path={Extremism.link} element={<Extremism />} />
+          <Route path={About.link} element={<About />} />
+          <Route path={Drones.link} element={<Drones />} />
+        </Routes>
+      </Router>
+    </main>
+    <div className={"app"}>
+    </div>
+  </>);
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import { Component } from "react";
+import NavBar from "./NavBar";
 
 export default class Header extends Component {
-    render() {
-        return (
-            <header>
-                <h1>Школа №41</h1>
-                <p>Будущее за теми, кто учится!</p>
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header>
+        <nav>
+          <h1>Школа №41</h1>
+          <p>Будущее за теми, кто учится!</p>
+        </nav >
+        <NavBar />
+      </header>
+    )
+  }
 
 }
