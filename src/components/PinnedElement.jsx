fb88444fe32c5b057c0fa26c2df9d249d85f6edc
Ivan Lukovkin <iamlukovkin@Ivans-MacBook-Pro.local>
2025-04-02 17:25:01 +0300

    Refactoring: change sitebar

---
+++ src/components/PinnedElement.jsx
@@ -0,0 +1,7 @@
+import pobeda from '../static/images/pobeda.jpg';
+
+export default function PinnedElement() {
+  return (<>
+    <img src={pobeda} alt={''} style={{ marginRight: 'auto', marginLeft: 'auto', width: '60%' }} />
+  </>);
+}
